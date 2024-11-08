e4526c08d710064f328c3bc83ed196f2e677322f
gabrosendo <gabygustorosendo@gmail.com>
2024-11-08 01:26:49 +0000

    Conclusão de projeto

---
+++ App.js
@@ -0,0 +1,52 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  const [peso,setPeso] = useState(0);
+  const [altura,setAltura] = useState(0);
+  const [imc,setImc] = useState(null);
+
+  const calcularImc = () =>{
+    var resultado = peso / (altura * altura);
+    setImc(resultado);
+  }
+
+/* */
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Aplicativo Cálculo IMC</Text>
+      <TextInput
+      placeholder="Peso"
+      keyboardType="numeric"
+      value={peso}
+      onChangeText={(peso)=>setPeso(peso)}
+      />
+
+<TextInput
+      placeholder="Altura"
+      keyboardType="numeric"
+      value={altura}
+      onChangeText={(altura)=>setAltura(altura)}
+      />
+    {imc&&(
+      <Text style={styles.titulo}>Imc: {imc}</Text>
+    )}
+    <Button title="Calcular" onPress={()=>{calcularImc()} }/>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titulo:{
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
+});
